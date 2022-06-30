38244045c2a93b3ef429556db4f028e54d46116c
Dominik Cvetko <docvetko@gmail.com>
2022-06-30 21:32:39 +0200

    promjene s postavkama

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import font
-
+from date_time.datetime_utils import *
 from lights.gui import  LightsModule
 from security.gui import SecurityModule
 from temperature.gui import TemperatureModule
@@ -14,6 +14,7 @@ class MySmartHome(tk.Tk):
         self.title("Algebra | MySmartHome")
         self.geometry("800x600")
         self.configure(background='black')
+        self.create_time_frame()
 
         self.apply_custom_style()
         self.create_tab_navigation()
@@ -45,7 +46,7 @@ class MySmartHome(tk.Tk):
 
     def create_tab_navigation(self):
         self.notebook = ttk.Notebook(self)
-        self.notebook.grid(row=0, column=0 )
+        self.notebook.grid(row=1, column=0 )
 
         self.tv_frame = TVModule(self, background='Black')
         self.tv_frame.grid(row=0, column=1)
@@ -63,9 +64,33 @@ class MySmartHome(tk.Tk):
         self.temperature_frame.grid(row=0, column=1)
         self.notebook.add(self.temperature_frame, text="Temperature")
 
-        self.temperature_frame = SettingsModule(self,  background='Black')
-        self.temperature_frame.grid(row=0, column=1)
-        self.notebook.add(self.temperature_frame, text="Settings")
+        self.settings_frame = SettingsModule(self,  background='Black')
+        self.settings_frame.grid(row=0, column=1)
+        self.notebook.add(self.settings_frame, text="Settings")
+
+    def create_time_frame(self):
+        time_frame = tk.LabelFrame(self,text='', width=100, height=100, background='black', fg='#ffffff')
+        time_frame.grid(column=0, row=0, padx=0, pady=0)
+
+        date_label = tk.Label(
+            time_frame, text=formatted_date(),
+            font=('Arial', 12), background='black', fg='#ffffff'
+            )
+        date_label.grid(row=0, column=0, padx=10, pady=10)
+
+        time_label = tk.Label(
+            time_frame, text=formatted_time(),
+            font=('Arial', 12), background='black', fg='#ffffff'
+            )
+        time_label.grid(row=0, column=1, padx=15, pady=15)
+
+        def refresh_time():
+            now_time = formatted_time()
+            time_label.configure(text=now_time)
+            time_label.after(1000, refresh_time)
+
+        refresh_time() 
+
 
 root = MySmartHome()
 
--- settings/gui.py
+++ settings/gui.py
@@ -1,13 +1,35 @@
+
 import tkinter as tk
+from tkinter import IntVar
+from tkinter import ttk
+from tkinter.colorchooser import askcolor
 
 
 class SettingsModule(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
-        self.font = ("Arial", 20)
+        self.font = ("Arial", 12)
 
         self.create_test_label()
+        self.create_color_button()
+        self.create_time_check()
+
     
     def create_test_label(self):
         self.test_label = tk.Label(self, text="Settings", font=self.font, background= "black", foreground="white")
         self.test_label.grid(row=0, column=0)
+    
+    def create_color_button(self):
+        self.color_label = tk.Button(self, text="Select a Color", font=self.font, background= "black", foreground="white", command=self.change_color)
+        self.color_label.grid(row=1, column=0)
+    
+    def create_time_check(self):
+        CheckVar1 = IntVar()
+        self.test_label = tk.Checkbutton(self, text="Show current time", font=self.font, background= "black", foreground="white",variable = CheckVar1, \
+                 onvalue = 1, offvalue = 0, height=5, \
+                 width = 20)
+        self.test_label.grid(row=2, column=0)
+
+    def change_color(self):
+        self.colors = askcolor(title="Background Color Chooser")
+        self.configure(bg=self.colors[1])
