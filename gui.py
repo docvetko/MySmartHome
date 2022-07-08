3a13e36e41343cd6fb6223964b8df9378cb065e5
Dominik Cvetko <docvetko@gmail.com>
2022-07-08 21:32:59 +0200

    final

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import tkinter as tk
 from tkinter import ttk
 from tkinter import font
@@ -8,6 +9,19 @@ from temperature.gui import TemperatureModule
 from tv.gui import TVModule
 from meteo.gui import MeteoModule
 
+
+def formatted_date():
+
+    today = dt.datetime.now()
+
+    return today.strftime('%A, %d %B %Y')
+    
+def formatted_time():
+
+    now_time = dt.datetime.now()
+
+    return now_time.strftime('%H : %M : %S')
+
 class MySmartHome(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -17,6 +31,7 @@ class MySmartHome(tk.Tk):
 
         self.apply_custom_style()
         self.create_tab_navigation()
+        self.create_time_frame()
     
     def apply_custom_style(self):
         self.style = ttk.Style()
@@ -45,28 +60,50 @@ class MySmartHome(tk.Tk):
 
     def create_tab_navigation(self):
         self.notebook = ttk.Notebook(self)
-        self.notebook.grid(row=0, column=0 )
+        self.notebook.grid(row=1, column=0 )
 
         self.tv_frame = TVModule(self, background='Black')
-        self.tv_frame.grid(row=0, column=1)
+        self.tv_frame.grid(row=1, column=1)
         self.notebook.add(self.tv_frame, text="TV")
 
         self.lights_frame = LightsModule(self, background='Black')
-        self.lights_frame.grid(row=0, column=1)
+        self.lights_frame.grid(row=1, column=1)
         self.notebook.add(self.lights_frame, text="Lights")
 
         self.security_frame = SecurityModule(self, background='Black')
-        self.security_frame.grid(row=0, column=1)
+        self.security_frame.grid(row=1, column=1)
         self.notebook.add(self.security_frame, text="Security")
 
         self.temperature_frame = TemperatureModule(self,  background='Black')
-        self.temperature_frame.grid(row=0, column=1)
+        self.temperature_frame.grid(row=1, column=1)
         self.notebook.add(self.temperature_frame, text="Temperature")
 
         self.meteo_frame = MeteoModule(self,  background='Black')
-        self.meteo_frame.grid(row=0, column=1)
+        self.meteo_frame.grid(row=1, column=1)
         self.notebook.add(self.meteo_frame, text="Meteo")
 
+    def create_time_frame(self):
+        #time_frame = tk.LabelFrame(self,text='', width=100, height=100, background='black', fg='#ffffff')
+        #time_frame.grid(column=0, row=0, padx=0, pady=0)
+
+        date_label = tk.Label(
+            self, text=formatted_date(),
+            font=('Arial', 12), background='black', fg='#ffffff'
+            )
+        date_label.grid(row=0, column=0, padx=5, pady=5)
+
+        time_label = tk.Label(
+            self, text=formatted_time(),
+            font=('Arial', 12), background='black', fg='#ffffff'
+            )
+        time_label.grid(row=0, column=2,  padx=5, pady=5)
+
+        def refresh_time():
+            now_time = formatted_time()
+            time_label.configure(text=now_time)
+            time_label.after(1000, refresh_time)
+
+        refresh_time() 
 
 root = MySmartHome()
 
--- meteo/gui.py
+++ meteo/gui.py
@@ -1,50 +1,61 @@
 import tkinter as tk
+from meteo.get_meteo import Prognoza
 
-import WeatherForecast
+
 
 class MeteoModule(tk.Frame):
-    
+
+
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.font = ("Arial", 20)
 
         self.create_test_label()
+        self.create_location_label()
+        self.create_temp_label()
+        self.create_hum_label()
+        self.create_press_label()
+        self.create_refresh_label()
     
+   
     def create_test_label(self):
+        
         self.test_label = tk.Label(self, text="Meteo", font=self.font, background= "black", foreground="white")
         self.test_label.grid(row=0, column=0)
     
     def create_location_label(self):
-        self.test_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
-        self.test_label.grid(row=1, column=0)
-
+        weather = Prognoza()
+        temperatue_data = weather.get_formated_weather_data()
+        self.location_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
+        self.location_label.grid(row=1, column=0)
+        self.location_label.configure(text=temperatue_data['location'])
+        
     def create_temp_label(self):
-        self.test_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
-        self.test_label.grid(row=2, column=0)
+        weather = Prognoza()
+        temperatue_data = weather.get_formated_weather_data()
+        self.temp_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
+        self.temp_label.grid(row=2, column=0)
+        self.temp_label.configure(text=f"{temperatue_data['current_temperature']} °C")
     
     def create_hum_label(self):
-        self.test_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
-        self.test_label.grid(row=3, column=0)
+        weather = Prognoza()
+        temperatue_data = weather.get_formated_weather_data()
+        self.hum_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
+        self.hum_label.grid(row=3, column=0)
+        self.hum_label.configure(text=f" Vlažnost: {temperatue_data['humidity']}")
 
     def create_press_label(self):
-        self.test_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
-        self.test_label.grid(row=3, column=0)    
+        weather = Prognoza()
+        temperatue_data = weather.get_formated_weather_data()
+        self.press_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
+        self.press_label.grid(row=4, column=0)
+        self.press_label.configure(text=f" Tlak: {temperatue_data['pressure']}")   
 
     def create_refresh_label(self):
-            self.test_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
-            self.test_label.grid(row=4, column=0) 
-            
-    def refresh_temperature():
-        
-        weather = WeatherForecast()
+        weather = Prognoza()
         temperatue_data = weather.get_formated_weather_data()
-        location_label.configure(text=temperatue_data['location'])
-        temp_label.configure(text=f"{temperatue_data['current_temperature']} °C")
-        hum_label.configure(text=temperatue_data['humidity'])
-        press_label.configure(text=temperatue_data['humidity'])
-        last_refresh_label.configure(text=temperatue_data['last_refrersh'])
-        location_label.after(1000 * 10, refresh_temperature)
-    
-    refresh_temperature()
-
+        self.refresh_label = tk.Label(self, text="", font=self.font, background= "black", foreground="white")
+        self.refresh_label.grid(row=5, column=0)
+        self.refresh_label.configure(text=temperatue_data['last_refrersh'])
+            
 
